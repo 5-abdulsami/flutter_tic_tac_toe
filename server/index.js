a08937823633f5d8ad49991b396ccf03fee8c8fd
5-abdulsami <5abdulsami2004@gmail.com>
2024-12-03 22:07:40 +0500

    created the node js server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log("Abdul Sami");
+const express = require("express");
+const http = require("http");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+var server = http.createServer(app);
+
+var io = require("socket.io")(server);
+
+// middle ware
+app.use(express.json());
+
+server.listen(port, '0.0.0.0', () => {
+    console.log(`server is running on ${port}`);
+});
